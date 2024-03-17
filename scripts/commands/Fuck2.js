280b20f72779a5165279e9a17d967dc00e91097a
OM E <161785462+OME06@users.noreply.github.com>
2024-03-17 07:00:07 +0600

    Update Fuck2.js

--- scripts/commands/Fuck2.js
+++ scripts/commands/Fuck2.js
@@ -7,7 +7,7 @@ module.exports.config = {
     name: "fuckv2",
     version: "2.0.0",
     permssion: 2,
-    credits: "Mahabub Rahaman",
+    credits: " OME BOSS",
     description: "",
     prefix: true,
     category: "fuckv2",
